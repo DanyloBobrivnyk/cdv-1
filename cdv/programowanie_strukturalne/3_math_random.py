7b9157a6a448f405d6b5e696f4eb59f38ab006e7
sczajkowski <48910937+sczajkowski@users.noreply.github.com>
2019-04-16 15:13:09 +0200

    Add files via upload

---
+++ cdv/programowanie_strukturalne/3_math_random.py
@@ -0,0 +1,24 @@
+# liczba pi
+import math
+pi = math.pi
+print(pi)
+
+# pierwiastek
+pierw = math.sqrt(9)
+print(pierw)
+
+#moduł random
+
+import random
+losuj = random.random()
+print(losuj)
+
+losujZlisty = random.choice([1,2,33,4])
+print(losujZlisty)
+
+'''
+Użytkownik podaje z klawiatuy 2 wartości, min i maks z przedziału z którego będzie losowane 5 liczb całkowitych
+Liczby wyświetl w formacie Liczba 1... Liczba 2... Liczba 3... (DO DOMU)
+'''
+
+#instrukcje warunkowe
---
+++ cdv/programowanie_strukturalne/4_instrukcje_warunkowe.py
@@ -0,0 +1,7 @@
+x = 6
+if x==5:
+    print("x=5")
+    print("x=5")
+    print("x=5")
+else:
+    print("x nie jest równe 5")
